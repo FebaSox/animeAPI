f28dcf1dd51bd6778c228ae321be87cc873c9b4c
Seba Fox <seba.n.fox@gmail.com>
2024-06-27 12:57:52 -0400

    testing with react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HomePage from './HomePage.js'
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+    <h1>Learning React</h1>
+    <p>Para</p>
+    <HomePage />
+    </>
+  )
+}
+
+
+
+export default App
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <>
+      <h1> React </h1>
+      <form id="form">
+        <label for="form">Form</label> 
+        <input type="test" id="input" />
+        <button type="submit">Submit</button> 
+      </form>
+    </>
+  );
+}
+
+export default HomePage
